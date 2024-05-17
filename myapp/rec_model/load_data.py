becfc6eb5ca649321e8b662affc7e04a3ca2a794
M4ks1m123 <maximalian67@gmail.com>
2024-05-17 17:04:40 +0300

    added load_data.py

--- myapp/rec_model/load_data.py
+++ myapp/rec_model/load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    ratings, books = pd.read_csv('C:/Users/Caster/Desktop/django_app/mysite/myapp/rec_model/data/Ratings.csv'), pd.read_csv('C:/Users/Caster/Desktop/django_app/mysite/myapp/rec_model/data/Books.csv')
+    ratings, books = pd.read_csv('C:/Users/Caster/Desktop/django_app/mysite/myapp/rec_model/data/Ratings.csv', dtype='unicode'), pd.read_csv('C:/Users/Caster/Desktop/django_app/mysite/myapp/rec_model/data/Books.csv', dtype='unicode')
     
     books = books.drop(['Book-Author','Year-Of-Publication','Image-URL-S','Image-URL-M','Image-URL-L'], axis=1)
     books = books.rename(columns={'ISBN':'bookId', 'Book-Title':'title', 'Publisher': 'genres'})
@@ -10,3 +10,7 @@ def load_data():
     ratings['rating'] = ratings['rating'].astype('float64')
     
     return ratings, books
+
+ratings, books = load_data()
+
+print(ratings.size)
--- myapp/rec_model/model_utils.py
+++
@@ -1,5 +0,0 @@
-from model_build import predict_books
-
-similarity, results_df = predict_books('book_embed', 8, 10, 100000)
-
-print(results_df.title[:10].tolist())

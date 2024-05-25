723c954f3bd044380db89b7f9a4b34d7006cdb45
M4ks1m123 <maximalian67@gmail.com>
2024-05-25 20:53:48 +0300

    fixed urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("recommendations/", views.recommendations, name="Recommendations")
+    path("recommendations/", views.recommendations, name="Recommendations"),
+    path("recommendations_json/", views.recommendations_json, name="Recommendations")
 ]

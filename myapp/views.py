d9d6db13cb096a19783f2b3a1d107707c53c58c6
M4ks1m123 <maximalian67@gmail.com>
2024-05-25 20:51:01 +0300

    added css

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from myapp.rec_model.model_build import predict_books
+from django.http import HttpResponse, JsonResponse
 
 def home(request):
     return render(request, "home.html")
@@ -10,5 +11,10 @@ def recommend_movies():
     return results_df.title[:10].tolist()
 
 
+def recommendations_json(request):
+    # return render(request, "recommendations.html", {"books": recommend_movies()})
+    return JsonResponse({"books": recommend_movies()})
+
 def recommendations(request):
-    return render(request, "recommendations.html", {"books": recommend_movies()})
+    return render(request, "myapp/recommendations.html", {"books": recommend_movies(), "id": 1})
+    # return JsonResponse({"books": recommend_movies()})
